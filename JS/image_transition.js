73a189e4d7db399457f24094268cb3cc1ecdc480
Shivansh Shalabh <shalabhshivansh@gmail.com>
2024-08-15 16:02:05 -0400

    Updated Portfolio

--- JS/image_transition.js
+++ JS/image_transition.js
@@ -1,12 +1,12 @@
-let rolesImgElems = document.querySelectorAll('.roles-blob img')
-let rolesImages = ['web_developer.svg', 'technopreneur.svg','competetive_coder.svg']
+let rolesImgElems = document.querySelectorAll('.roles-blob img');
+let rolesImages = ['web_developer.svg', 'researcher.svg', 'technopreneur.svg', 'competetive_coder.svg'];
 let roleImageCounter = 1;
-let roleImgSrcPath = rolesImgElems[0].getAttribute('data-img-src')
+let roleImgSrcPath = rolesImgElems[0].getAttribute('data-img-src');
 const changeRoleImage = () => {
     roleImageCounter++;
     if (roleImageCounter === rolesImages.length) roleImageCounter = 0;
     let roleFirstImageHasClass = rolesImgElems[0].classList.contains('active-role-img');
-    rolesImgElems[roleFirstImageHasClass?1:0].classList.add('active-role-img');
-    rolesImgElems[roleFirstImageHasClass?0:1].classList.remove('active-role-img');
-    rolesImgElems[roleFirstImageHasClass?0:1].src = roleImgSrcPath + rolesImages[roleImageCounter];
-}
+    rolesImgElems[roleFirstImageHasClass ? 1 : 0].classList.add('active-role-img');
+    rolesImgElems[roleFirstImageHasClass ? 0 : 1].classList.remove('active-role-img');
+    rolesImgElems[roleFirstImageHasClass ? 0 : 1].src = roleImgSrcPath + rolesImages[roleImageCounter];
+};
